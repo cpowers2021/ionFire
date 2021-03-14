4e09023ee257fc1617e250ce92f49ff29da83e1d
cpowers2021 <54912918+cpowers2021@users.noreply.github.com>
2021-03-14 12:53:22 -0700

    Feat: Set up the project for firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Firebase } from '@ionic-native/firebase/ngx';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireFunctionsModule } from '@angular/fire/functions';
+import { AngularFireMessagingModule } from '@angular/fire/messaging';
+
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  AngularFireModule.initializeApp(environment.firebase), AngularFirestoreModule,
+  AngularFireMessagingModule, AngularFireFunctionsModule, AngularFireAuthModule],
+  providers: [{ provide: {RouteReuseStrategy, Firebase}, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBYJ4nyVvadEnPkfV_V6GgoncJzjgwfBuQ",
+        authDomain: "ionic-4-fire-f54fb.firebaseapp.com",
+        projectId: "ionic-4-fire-f54fb",
+        storageBucket: "ionic-4-fire-f54fb.appspot.com",
+        messagingSenderId: "432394406834",
+        appId: "1:432394406834:web:c232c7be2bf1c2aa0ab2ab",
+        measurementId: "G-LNQRFZWM0F"
+    }
 };
