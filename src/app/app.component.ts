4aa7d251280ca3efc3f591fbd16ee040168f7f03
cpowers2021 <54912918+cpowers2021@users.noreply.github.com>
2021-03-15 18:37:28 -0700

    Feat: Create an initialization sequence for the website with a status bar and splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './services/auth.service';
+import { Storage } from '@ionic/storage';
+import { Router } from '@angular/router';
+import { FcmService } from './services/fcm.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +13,19 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(private platform: Platform, private splashScreen: SplashScreen,
+  private statusBar: StatusBar, private menu: MenuController) {
+  this.initializeApp();
+  }
+  initializeApp() {
+  this.platform.ready().then(() => {
+    this.statusBar.styleDefault();
+    this.splashScreen.hide();
+    this.fcm.getPermission().subscribe();
+    this.fcm.listenToMessages().subscribe();
+  )}
+  }
+  closeMenu() {
+    this.menu.close();
+  }
 }
