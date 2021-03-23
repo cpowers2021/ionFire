227f71387b18c5ef0b0a341d3c56dab7a074ac86
cpowers2021 <54912918+cpowers2021@users.noreply.github.com>
2021-03-23 08:32:14 -0700

    Feat: Set up redirect auth for google sign-in on the web

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -13,7 +13,9 @@ import { DbService } from './db.service';
 export class AuthService {
   user$: Observable<any>;
 
-  constructor(private afAuth: AngularFireAuth, private db: DbService, private router: Router) {
+  constructor(private afAuth: AngularFireAuth, private db: DbService, private router: Router,
+  private gplus: GooglePlus, private platform: Platform, private loadingController: LoadingController,
+  private storage: Storage) {
     this.user$ = this.afAuth.authState.pipe(
     switchMap(user => (user ? db.doc$(`users/${user.uid}`) : of(null)))
     );
@@ -34,4 +36,55 @@ export class AuthService {
     };
     return this.db.updateAt(path, data);
   }
+
+  async signOut() {
+    await this.afAuth.auth.signOut();
+    return this.router.navigate(['/']);
+  }
+
+  //Google Auth
+
+  setRedirect(val) {
+    this.storage.set('authRedirect', val);
+  }
+
+  async isRedirect() {
+    return await this.storage.get('authRedirect');
+  }
+
+  async googleLogin() {
+    try {
+      let user;
+
+      if(this.platform.is('cordova')) {
+        user = await this.nativeGoogleLogin();
+      } else {
+        await this.setRedirect(true);
+        const provider = new auth.GoogleAuthProvider();
+        user = await this.afAuth.auth.signInWithRedirect(provider);
+      }
+      return await this.updatedUserData(user);
+    } catch(err) {
+          console.log(err);
+        }
+}
+
+//Handle login with redirect for web Google auth
+private async handleRedirect() {
+  if((await this.isRedirect()) !== true) {
+    return null;
+  }
+  const loading = await this.loadingController.create();
+  await loading.present();
+
+  const result = await this.afAuth.auth.getRedirectResult();
+
+  if(result.user) {
+    await this.updateUserData(result.user);
+  }
+  await loading.dismiss();
+  await this.setRedirect(false);
+  return result;
+}
+
 }
